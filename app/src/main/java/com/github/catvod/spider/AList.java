1c400a7b5110ae8226a9cbaac7c628048d7adebb
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-05 16:11:04 +0800

    Update AList.java

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -52,8 +52,8 @@ public class AList extends Spider {
     private void fetchRule() {
         if (drives != null && !drives.isEmpty()) return;
         if (ext.startsWith("http")) ext = OkHttp.string(ext);
-        ext = "{\"drives\":" + ext + "}";
-        Drive drive = Drive.objectFrom(ext);
+        ext1 = "{\"drives\":" + ext + "}";
+        Drive drive = Drive.objectFrom(ext1);
         drives = drive.getDrives();
         vodPic = drive.getVodPic();
     }

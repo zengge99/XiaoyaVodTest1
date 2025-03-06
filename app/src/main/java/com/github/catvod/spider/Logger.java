d51f1dd6a34d7444ea772f72816c6884ba3e4513
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-06 09:40:17 +0800

    Update Logger.java

--- app/src/main/java/com/github/catvod/spider/Logger.java
+++ app/src/main/java/com/github/catvod/spider/Logger.java
@@ -14,7 +14,7 @@ public class Logger {
         String callPrefix = "";
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
         if (stackTrace.length >= 3) {
-            StackTraceElement caller = stackTrace[2];
+            StackTraceElement caller = stackTrace[1];
             String className = caller.getClassName();
             String methodName = caller.getMethodName();
             int lineNumber = caller.getLineNumber();

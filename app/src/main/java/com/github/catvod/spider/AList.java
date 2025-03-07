1cfa2a248b556f5164b542a206446fd4d4a297da
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-07 17:59:37 +0800

    Update AList.java

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -77,9 +77,9 @@ public class AList extends Spider {
         try {
             String urlParams = convertToUrlParams(new JSONObject(param));
             url = url + "?" + urlParams;
-            url = "http://127.0.0.1:9988/proxy?do=gen&thread=0&url=" + URLEncoder.encode(url);
-            Logger.log(url);
-            String response = OkHttp.post(url, drive.getHeader());
+            JSONObject params = convertFromUrlParams(do=gen&thread=0&url=" + URLEncoder.encode(url));
+            Logger.log(params);
+            String response = OkHttp.post("http://127.0.0.1:9988/proxy", params.toString(), drive.getHeader()).getBody();
             Logger.log(response);
             SpiderDebug.log(response);
             if (retry && response.contains("Guest user is disabled") && login(drive))

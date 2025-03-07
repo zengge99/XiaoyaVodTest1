d8264baa7c47551837381e1dffb7028eac2cb142
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-07 20:28:15 +0800

    Update XiaoyaProxyHandler.java

--- app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
+++ app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
@@ -135,7 +135,8 @@ public class XiaoyaProxyHandler {
                 }
 
                 Map<String, String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-                List<String> keys = Arrays.asList("referer", "icy-metadata", "range", "connection", "accept-encoding", "user-agent", "cookie", "authorization");
+                //List<String> keys = Arrays.asList("referer", "icy-metadata", "range", "connection", "accept-encoding", "user-agent", "cookie", "authorization");
+                List<String> keys = Arrays.asList("referer", "icy-metadata", "range", "connection", "user-agent", "cookie", "authorization");
                 for (String key : params.keySet()) if (keys.contains(key)) headers.put(key, params.get(key));
                 if(url.contains("夸克")) {
                     headers.put("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) quark-cloud-drive/2.5.20 Chrome/100.0.4896.160 Electron/18.3.5.4-b478491100 Safari/537.36 Channel/pckk_other_ch");

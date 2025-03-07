7409d5682e39f50c1493485aba4735ce64121be9
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-07 18:57:24 +0800

    Update AList.java

--- app/src/main/java/com/github/catvod/spider/AList.java
+++ app/src/main/java/com/github/catvod/spider/AList.java
@@ -81,7 +81,7 @@ public class AList extends Spider {
             Logger.log(url);
             Logger.log(URLEncoder.encode(url));
             JSONObject params = convertFromUrlParams("do=gen&thread=0&url=" + URLEncoder.encode(url));
-            Logger.log(params);
+            Logger.log(params.toString());
             String response = OkHttp.post("http://127.0.0.1:9988/proxy", params.toString(), drive.getHeader()).getBody();
             Logger.log(response);
             SpiderDebug.log(response);

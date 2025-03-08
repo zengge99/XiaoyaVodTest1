b65ac2aeb27238bbe095c1fc922dd039dc252eff
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-08 18:22:25 +0800

    Create DoubanInfo.java

---
+++ app/src/main/java/com/github/catvod/bean/DoubanInfo.java
@@ -0,0 +1,78 @@
+package com.github.catvod.bean;
+
+public class DoubanInfo {
+        private String id;
+        private String plot;
+        private String year;
+        private String region;
+        private String actors;
+        private String director;
+        private String type;
+        private String rating; 
+
+        // Getter 方法
+        public String getId() {
+            return id != null ? id : "";
+        }
+        
+        public String getPlot() {
+            return plot != null ? plot : "";
+        }
+    
+        public String getYear() {
+            return year != null ? year : "";
+        }
+    
+        public String getRegion() {
+            return region != null ? region : "";
+        }
+    
+        public String getActors() {
+            return actors != null ? actors : "";
+        }
+    
+        public String getDirector() {
+            return director != null ? director : "";
+        }
+    
+        public String getType() {
+            return type != null ? type : "";
+        }
+    
+        public String getRating() {
+            return rating != null ? rating : "";
+        }
+    
+        // Setter 方法
+        public void setId(String id) {
+            this.id = id;
+        }
+        
+        public void setPlot(String plot) {
+            this.plot = plot;
+        }
+    
+        public void setYear(String year) {
+            this.year = year;
+        }
+    
+        public void setRegion(String region) {
+            this.region = region;
+        }
+    
+        public void setActors(String actors) {
+            this.actors = actors;
+        }
+    
+        public void setDirector(String director) {
+            this.director = director;
+        }
+    
+        public void setType(String type) {
+            this.type = type;
+        }
+    
+        public void setRating(String rating) {
+            this.rating = rating;
+        }
+    }

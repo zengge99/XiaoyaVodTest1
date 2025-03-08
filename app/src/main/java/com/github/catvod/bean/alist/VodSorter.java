66133295bd27767b49ba645cfd40a64900429d1b
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-08 22:16:32 +0800

    Update VodSorter.java

--- app/src/main/java/com/github/catvod/bean/alist/VodSorter.java
+++ app/src/main/java/com/github/catvod/bean/alist/VodSorter.java
@@ -77,10 +77,7 @@ public class VodSorter {
     }
     
     private static List<Vod> getRandomElements(List<Vod> source, int count, boolean keepOrder) {
-        Logger.log(count);
         if (source.size() <= count) {
-            Logger.log("随机化");
-            Logger.log(keepOrder);
             List<Vod> result = new ArrayList<>(source);
             if (!keepOrder) Collections.shuffle(result);
             return result;

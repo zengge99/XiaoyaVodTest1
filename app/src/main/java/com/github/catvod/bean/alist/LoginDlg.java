c4b041bd6c0c61a8d94e69d7a0592e99ef252744
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-12 16:26:35 +0800

    Update LoginDlg.java

--- app/src/main/java/com/github/catvod/bean/alist/LoginDlg.java
+++ app/src/main/java/com/github/catvod/bean/alist/LoginDlg.java
@@ -12,14 +12,26 @@ import android.widget.Toast;
 
 public class LoginDlg {
 
-    public static void showLoginDlg() {
+    private static boolean isBusy = false;
+    private static String userInput = "";
+
+    public static boolean getIsBusy() {
+        return LoginDlg.isBusy;
+    }
+
+    public static void setIsBusy(boolean isBusy) {
+        LoginDlg.isBusy = isBusy;
+    }
+
+    public static String showLoginDlg(String hint) {
         try {
             Activity activity = Init.getActivity();
             Init.run(() -> {
+                LoginDlg.setIsBusy(true);
                 if (activity != null && !activity.isFinishing() && !activity.isDestroyed()) {
                     // 创建一个 EditText 用于用户输入
                     final EditText input = new EditText(activity);
-                    input.setHint("请输入内容");
+                    input.setHint(hint);
 
                     // 创建 AlertDialog
                     AlertDialog.Builder builder = new AlertDialog.Builder(activity);
@@ -30,13 +42,8 @@ public class LoginDlg {
                             .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
-                                    // 获取用户输入
-                                    String userInput = input.getText().toString();
-                                    if (!userInput.isEmpty()) {
-                                        Toast.makeText(activity, "您输入的内容是：" + userInput, Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        Toast.makeText(activity, "输入不能为空", Toast.LENGTH_SHORT).show();
-                                    }
+                                    LoginDlg.userInput = input.getText().toString();
+                                    LoginDlg.userInput = LoginDlg.userInput == null ? "" : LoginDlg.userInput;
                                 }
                             })
                             .setNegativeButton("取消", new DialogInterface.OnClickListener() {
@@ -50,9 +57,14 @@ public class LoginDlg {
                     // 显示对话框
                     AlertDialog dialog = builder.create();
                     dialog.show();
+                    LoginDlg.setIsBusy(false);
                 }
             }, 500);
         } catch (Exception e) {
+        }
+
+        while (LoginDlg.getIsBusy()) {
+            Thread.sleep(50);
         }
     }
 }

268fc29ce0520ff0877d2356a6c851126df844ed
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-12 16:29:48 +0800

    Update XiaoyaProxyHandler.java

--- app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
+++ app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
@@ -501,8 +501,8 @@ public class XiaoyaProxyHandler {
             case "gen":
                 return genProxy(params);
             case "login":
-                LoginDlg.showLoginDlg();
-                return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream("ok".getBytes("UTF-8"))};
+                LoginDlg.showLoginDlg("请输入内容");
+                return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream(LoginDlg.getUserInput().getBytes("UTF-8"))};
             default:
                 return Proxy.proxy(params);
         }
